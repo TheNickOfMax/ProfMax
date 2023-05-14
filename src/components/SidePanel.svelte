a250fcd93e7fe11e8d7d73f2311a306f43097ed9
LaPepega <tmv03092008@gmail.com>
2023-05-14 20:24:55 +0500

    SidePanel css fix

--- src/components/SidePanel.svelte
+++ src/components/SidePanel.svelte
@@ -9,25 +9,27 @@
 </div>
 
 <style lang="scss">
-	button {
-		background-color: #4d9785;
-		border-style: none;
-		border-top-right-radius: 50px;
-		border-bottom-right-radius: 50px;
-		margin-bottom: 5px;
-		height: fit-content;
-		font-size: larger;
-		padding-top: 5%;
-		padding-bottom: 5%;
-		padding-right: 20px;
-		padding-left: 10px;
-		transition: padding-left 0.5s ease;
-	}
 	.panel {
-		margin-top: 30px;
-		display: block;
+		button {
+			background-color: #4d9785;
+			border-style: none;
+			border-top-right-radius: 50px;
+			border-bottom-right-radius: 50px;
+			margin-bottom: 5px;
+			height: fit-content;
+			font-size: larger;
+			padding-top: 5%;
+			padding-bottom: 5%;
+			padding-right: 20px;
+			padding-left: 10px;
+			width: fit-content;
+			transition: padding-left 0.5s ease;
+		}
 		:hover {
 			padding-left: 50%;
 		}
+		margin-top: 30px;
+		display: flex;
+		flex-direction: column;
 	}
 </style>
