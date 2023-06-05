e41105fa82bd2dc4427ecf3809d2053a426e1079
LaPepega <tmv03092008@gmail.com>
2023-06-05 21:36:24 +0500

    Ytf don't dynamic imports work

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
+	import { SvelteComponent, onMount } from 'svelte';
+	let articles: any[] = [];
+
+	async function Mnt() {
+		let imports: Record<string, () => Promise<unknown>> = import.meta.glob('./articles/*.svelte');
+		for (let key in imports) {
+			let mod: any = await imports[key]();
+			const { default: Component } = mod;
+			articles.push(Component);
+		}
+	}
+	onMount(Mnt);
 </script>
 
 <head>
 	<title>Домашняя</title>
 </head>
-<div />
+<div>
+	{#each articles as article}
+		<svelte:component
+			this={article}
+			on:load={() => {
+				alert(article);
+			}}
+		/>
+	{/each}
+</div>
 
 <style lang="scss">
 </style>
