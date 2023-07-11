8c8e85029a61e567003e2499515abfe11356be5c
LaPepega <tmv03092008@gmail.com>
2023-07-12 02:06:13 +0500

    You should kiss chat gpt NOW!

--- src/components/SidePanel.svelte
+++ src/components/SidePanel.svelte
@@ -1,25 +1,19 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	
-	import { onMount, space } from 'svelte/internal';
-	const imp:any = import.meta.glob(`../routes/*/*/**.svelte`)
-	let curUrl:string ='';
-	let pathsToLoad:string[]=[];
-
+  
+	const importAll = import.meta.glob(`../routes/*/*/**.svelte`);
+	let pathsToLoad: string[] = [];
+  
 	$: {
-		curUrl = $page.url.toString().split('/')[3];
-		console.log(curUrl)
-		for (let path in imp) {
-			console.log('for')
-			if (path.includes(curUrl))
-			{
-				pathsToLoad.push(path.replace('/+page.svelte','').replace('../routes',''));
-			}
-    	}
+	  const currentUrl = $page.url.toString().split('/')[3];
+	  console.log(currentUrl);
+	  pathsToLoad = Object.keys(importAll)
+		.map((path) => path.replace('/+page.svelte', '').replace('../routes', ''))
+		.filter((path) => path.includes(currentUrl));
 	}
-
-	
-	</script>
+  </script>
+  
+  
 
 <div class="panel" >
 	{#each pathsToLoad as path}
@@ -31,27 +25,28 @@
 	.panel {
 		a {
 			background-color: #4d9785;
-			color: rgb(255, 255, 255);
-			text-decoration: none;
+			border-bottom-right-radius: 50px;
 			border-style: none;
 			border-top-right-radius: 50px;
-			border-bottom-right-radius: 50px;
-			margin-bottom: 5px;
-			height: fit-content;
+			color: rgb(255, 255, 255);
 			font-size: larger;
-			padding-top: 10px;
+			height: fit-content;
+			margin-bottom: 5px;
 			padding-bottom: 10px;
-			padding-right: 20px;
 			padding-left: 10px;
-			width: fit-content;
+			padding-right: 20px;
+			padding-top: 10px;
+			text-decoration: none;
 			transition: padding-left 0.5s ease;
 			white-space: nowrap;
+			width: fit-content;
 		}
 		:hover {
 			padding-left: 50%;
 		}
-		margin-top: 30px;
 		display: flex;
 		flex-direction: column;
+		margin-top: 30px;
 	}
 </style>
+
