75996dc8b0edeccb41ea004169dd5948de8e5198
LaPepega <tmv03092008@gmail.com>
2023-10-19 19:30:13 +0500

    Bottom bar fix

--- src/components/BottomBar.svelte
+++ src/components/BottomBar.svelte
@@ -13,10 +13,10 @@
 		width: 110%;
 		background-color: rgb(48, 48, 48);
 		position: fixed;
-		bottom: 0;
+		bottom: 0px;
 		left: 0;
 		font-size: small;
-		height: 40px;
+		height: fit-content;
 		p {
 			margin-left: 20px;
 			color: rgb(121, 121, 121);
