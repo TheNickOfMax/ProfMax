977296b7034380a6f2eb72d6df6d3ad79635042e
LaPepega <tmv03092008@gmail.com>
2023-10-25 22:35:19 +0500

    No Mila, yes Pasha

--- src/routes/about/Anntipenko Pavel/+page.svelte
+++ src/routes/about/Anntipenko Pavel/+page.svelte
@@ -1,5 +1,22 @@
-<div>
-	<h1>Информация про Пашу</h1>
-</div>
+<script lang="ts">
+	import SubbedText from '../../../components/SubbedText.svelte';
+	import Tile from '../../../components/Tile.svelte';
+</script>
 
-<style></style>
+<div>
+	<SubbedText mainText="Павел Антипенко" sub="Литейщик, человек электролобзик, или просто пашка." />
+	<div class="inside">
+		<Tile
+			title="Я Литейшик"
+			mainText="Ну или че там Паша придумает, я не знаю что писать про остальных."
+		/>
+		<Tile
+			title="Хобби"
+			mainText="Люблю пилить, стругать, печатать на 3D принтере и строить самолётики"
+		/>
+		<Tile
+			title="Что-то еще..."
+			mainText="Сюда надо еще чего-то добавить. Картинки пока не работают. Нужны идеи. "
+		/>
+	</div>
+</div>
