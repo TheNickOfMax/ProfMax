74b029861dd8414a17fcc776e4e52bee260a8a53
LaPepega <tmv03092008@gmail.com>
2023-11-02 17:58:48 +0500

    qna

--- src/components/Tile.svelte
+++ src/components/Tile.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	export let title: string = '';
 	export let mainText: string = '';
+	export let w: string = '250px';
 	let arr = mainText.split(/\n|\r\n/g).map((v, i) => {
 		return { text: v, br: '<br>' };
 	});
 </script>
 
-<div>
+<div style="width: {w};">
 	<h2>{title}</h2>
 	<p>
 		{#each arr as { text, br }}
@@ -19,7 +20,6 @@
 <style lang="scss">
 	div {
 		background-color: rgb(15, 15, 15);
-		width: 250px;
 		padding: 15px;
 		border-radius: 20px;
 		border-style: solid;
