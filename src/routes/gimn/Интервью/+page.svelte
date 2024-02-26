3d62f56414d2fb63978f52937d13c2999377f0fd
LaPepega <tmv03092008@gmail.com>
2024-02-26 20:19:45 +0500

    n

---
+++ src/routes/gimn/Интервью/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import SubbedText from '../../../components/SubbedText.svelte';
+</script>
+
+<head>
+	<title>Интервью</title>
+</head>
+
+<div>
+	<SubbedText mainText="Интервью с выпускниками гимназии." sub="" />
+</div>
