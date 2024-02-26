70c3eb3e21e061b896c749a3b4a5aab250a2ccda
LaPepega <tmv03092008@gmail.com>
2024-02-26 19:39:11 +0500

    I sold my soul to yandex

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,9 +1,41 @@
-<script lang="ts">
+<!-- Yandex.Metrika counter -->
+<script>
+	// @ts-nocheck
+	import { browser } from '$app/environment';
+	if (browser) {
+		(function (m, e, t, r, i, k, a) {
+			m[i] =
+				m[i] ||
+				function () {
+					(m[i].a = m[i].a || []).push(arguments);
+				};
+			m[i].l = 1 * new Date();
+			for (var j = 0; j < document.scripts.length; j++) {
+				if (document.scripts[j].src === r) {
+					return;
+				}
+			}
+			(k = e.createElement(t)),
+				(a = e.getElementsByTagName(t)[0]),
+				(k.async = 1),
+				(k.src = r),
+				a.parentNode.insertBefore(k, a);
+		})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+		ym(96579707, 'init', {
+			clickmap: true,
+			trackLinks: true,
+			accurateTrackBounce: true
+		});
+	}
 </script>
+
+<!-- /Yandex.Metrika counter -->
 
 <head>
 	<title>Главная</title>
 </head>
+
 <div>
 	<h1>Главная</h1>
 	<div>
@@ -41,6 +73,14 @@
 			</p>
 		</section>
 	</div>
+</div>
+
+<div>
+	<img
+		src="https://mc.yandex.ru/watch/96579707"
+		style="position:absolute; left:-9999px;"
+		alt="aa"
+	/>
 </div>
 
 <style lang="scss">
