2e262d02f3290decd13ad82a57ef471c1f90b482
LaPepega <tmv03092008@gmail.com>
2024-02-26 21:18:25 +0500

    Collapsible tile

---
+++ src/components/CollapsibleTile.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	let collapsed: boolean = true;
+	export let title: string;
+</script>
+
+<div
+	class="framed"
+	on:click={() => {
+		collapsed = !collapsed;
+	}}
+	on:keypress={() => {}}
+>
+	{#if collapsed}
+		<div class="inrow">
+			<img
+				src="/arrow-right.png"
+				width="25px"
+				height="25px"
+				style="filter: invert(1); margin-top:14px; margin-right:10px"
+				alt=""
+			/>
+			<h3>{title}</h3>
+		</div>
+	{:else}
+		<div class="inrow">
+			<img
+				src="/arrow-down.png"
+				width="25px"
+				height="25px"
+				style="filter: invert(1); margin-top:14px; margin-right:10px"
+				alt=""
+			/>
+			<h3>{title}</h3>
+		</div>
+		<slot />
+	{/if}
+</div>
